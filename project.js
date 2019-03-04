59897ef52f20a9551a15c9f22f597886666f12d5
Jean Turban <jt665@cornell.edu>
2019-03-03 19:18:35 -0500

    Add files via upload

--- project.js
+++ project.js
@@ -1,17 +1,20 @@
-var musicdata;
 
-// d3.csv("featuresdf.csv").then(function(data) {
+
+
+
 d3.csv("spotify.csv").then(function(data) {
 
-    musicdata = data;
+    let musicdata = data;
 
     //Select the div
     let q3 = d3.select("#viz");
-    q3.append("svg").attr("width", 800).attr("height", 800).attr("id","chart");
+    q3.append("svg").attr("width", 800).attr("height", 700).attr("id","chart");
     svg = d3.select("svg#chart");
 
+
+
     //Set Constant Values
-    margin = { "top": 50, "right": 50, "bottom": 50, "left":50};
+    margin = { "top": 20, "right": 50, "bottom": 80, "left":150};
     width = svg.attr("width");
     height = svg.attr("height");
     chartwidth = width - margin["left"] - margin["right"];
@@ -27,22 +30,85 @@ d3.csv("spotify.csv").then(function(data) {
     const loMin = d3.min(musicdata, d => d['loudness']);
     const loMax = d3.max(musicdata, d => d['loudness']);
 
-    console.log(duMax);
+    d['duration_ms'] = Number(d['duration_ms']);      
+    console.log(duMin)
+    console.log(duMax)
 
-    //Scales
-    yScale = d3.scaleLinear().domain([eMin, eMax]).range([chartheight,0]);
-    xScale = d3.scaleLinear().domain([0, 1000000]).range([0, chartwidth]);
+    //Scales for major viz
+    yScale = d3.scaleLinear().domain([1, 0]).range([0,chartheight]);
+    xScale = d3.scaleLinear().domain([0, 1000000/360]).range([0, chartwidth]);
     rScale = d3.scaleLinear().domain([loMin, loMax]).range([1, 7]);
     colorScale = d3.scaleSequential(d3.interpolateCool).domain([0, liMax])
+
+    //Legend
+    svgS = d3.select("#scales");
+
+    let initx = 0
+    let textx = 0
+    let inity = 25
+
+
+    svgS.append("text")
+      .attr("x", initx)
+      .attr("y",40)
+      .attr("fill","white")
+      .attr("font-size","20px")
+      .attr("class","font title")
+      .text("Spotify Song Data");
+
+    initx = initx + 250;
+    textx = textx + 250;
+    svgS.append("text")
+      .attr("x",initx)
+      .attr("y",inity-3)
+      .attr("fill","white")
+      .attr("font-size","14px")
+      .attr("class","font title")
+      .text("Song Liveness");
+    for(k=0; k <= 1;k=k+.2){
+      svgS.append("rect")
+        .attr("x", initx)
+        .attr("y", inity)
+        .attr("height", 20)
+        .attr("width", 20)
+        .attr("fill", colorScale(k));
+      svgS.append("text")
+        .attr("x", textx)
+        .attr("y","55")
+        .attr("fill","white")
+        .attr("font-size","10px")
+        .attr("class","font title")
+        .text(Math.round(k*10));
+      initx = initx + 20;
+      textx = textx + 21;
+    }
+    svgS.append("text")
+      .attr("x", initx+50)
+      .attr("y",40)
+      .attr("fill","white")
+      .attr("font-size","20px")
+      .attr("class","font title")
+      .text("X-Scale: Song Duration");
+
+    svgS.append("text")
+      .attr("x", initx+350)
+      .attr("y",40)
+      .attr("fill","white")
+      .attr("font-size","20px")
+      .attr("class","font title")
+      .text("Y-Scale: Song Energy");
+
+
+
 
     //Background
     svg.append("rect")
         .attr("x", margin["left"])
         .attr("y", margin["top"])
-        .attr("height", chartheight)
-        .attr("width", chartwidth)
+        .attr("height", chartwidth)
+        .attr("width", chartheight)
         .attr("id","viz-background")
-        .attr("fill", "#1c1c1c");
+        .attr("fill", "white");
 
     //Draw all circles
     svg.append("g").attr("id","datapoints")
@@ -51,16 +117,14 @@ d3.csv("spotify.csv").then(function(data) {
     for(k=0;k < musicdata.length;k++){
       if (musicdata[k].duration_ms != 'NaN' && musicdata[k].energy != 'NaN'){
         svgb2.append("circle")
-
-          .attr("cx",xScale(musicdata[k].duration_ms)+margin["left"])
-          .attr("cy",yScale(musicdata[k].energy)+margin["top"])
-          .attr("r",rScale(musicdata[k].loudness))
-          .style("fill", colorScale(musicdata[k].liveness))
-          .attr("opacity",.7);
-        }
-
+        .attr("cx",xScale(musicdata[k].duration_ms/360)+margin["left"])
+        .attr("cy",yScale(musicdata[k].energy)+margin["top"])
+        .attr("r",rScale(musicdata[k].loudness))
+        .style("fill", colorScale(musicdata[k].liveness))
+        .attr("id",musicdata[k].song_title)
+        .attr("opacity",.7);
+          }
     }
-
 
     leftAxis = d3.axisLeft(yScale);
     svg.append("g")
@@ -69,8 +133,6 @@ d3.csv("spotify.csv").then(function(data) {
       .style("font", "12px times")
       .attr("class","yscale")
       .call(leftAxis.ticks(5));
-
-
 
     bottomAxis = d3.axisBottom(xScale)
     svg.append("g")
@@ -82,24 +144,23 @@ d3.csv("spotify.csv").then(function(data) {
 
 
 
-      //NEW Vizualzation
+      //Artist1 Box
 
       svg = d3.select("#DrakeRec");
       svg.attr("width", 250).attr("height", 100).attr("id","info");
 
+      //Put the artists name here
       artists = "Drake"
-      console.log(musicdata)
-      top10 = musicdata.filter( d => d['artist'] == artists);
-      top10 = top10.filter( d => d['duration_ms'] != 'NaN');
+      drake = musicdata.filter( d => d['artist'] == artists);
+      drake = drake.filter( d => d['duration_ms'] != 'NaN');
 
-      console.log(top10);
-      //const test = d3.mean(musicdata, d => d['energy']);
-      //console.log(test)
-      const dmeanlo = d3.mean(top10, d => d['loudness']);
-      const dmeanli = d3.mean(top10, d => d['liveness']);
-      const dmeandu = d3.mean(top10, d => d['duration_ms']);
-      const dmeanen = d3.mean(top10, d => d['energy']);
+      //Mean Values of Columns for Drake
+      const dmeanlo = d3.mean(drake, d => d['loudness']);
+      const dmeanli = d3.mean(drake, d => d['liveness']);
+      const dmeandu = d3.mean(drake, d => d['duration_ms']);
+      const dmeanen = d3.mean(drake, d => d['energy']);
 
+      //Artists Name Print along with statistics
       svg.append("text")
         .attr("x","10")
         .attr("y","30")
@@ -123,104 +184,99 @@ d3.csv("spotify.csv").then(function(data) {
               .text("AVG energy:" + Math.abs((dmeanen)));
 
 
-      //Balls
+      //Smaller Viz of the big viz
       svg = d3.select("#drakeballs");
       svg.attr("width", 400).attr("height", 250);
 
-      musicdata = top10;
+      //MusicData
+      musicdata = drake;
 
+      //Margins and Chart Attr
       margin = { "top": 10, "right": 150, "bottom": 10, "left":10};
       width = svg.attr("width");
       height = svg.attr("height");
       chartwidth = width - margin["left"] - margin["right"];
       chartheight = height - margin["top"]-margin["bottom"];
 
+      //New Scales for smaller viz
       yScale = d3.scaleLinear().domain([1, 0]).range([0,230]);
       xScale = d3.scaleLinear().domain([0, 1000000]).range([0, 230]);
       rScale = d3.scaleLinear().domain([loMin, loMax]).range([1, 3]);
       colorScale = d3.scaleSequential(d3.interpolateCool).domain([0, liMax])
 
-
+      //Append a group with the data points
       svg.append("g").attr("id","drakestuffs")
       svgb2 = d3.select("g#drakestuffs");
 
       for(k=0;k < musicdata.length;k++){
         if (musicdata[k].duration_ms != 'NaN' && musicdata[k].energy != 'NaN'){
           svgb2.append("circle")
-
           .attr("cx",xScale(musicdata[k].duration_ms)+margin["left"])
           .attr("cy",yScale(musicdata[k].energy)+margin["top"])
           .attr("r",rScale(musicdata[k].loudness))
           .style("fill", colorScale(musicdata[k].liveness))
-          .attr("opacity",.7);
-            }
-
+          .attr("opacity",1);
+          }
       }
-
 
       leftAxis = d3.axisLeft(yScale);
       svg.append("g")
         .attr("stroke-width",1.8)
         .attr("transform","translate("+(margin.left)+","+margin.top+")")
         .style("font", "12px times")
-        .attr("class","yscale")
+        .attr("color","white")
         .call(leftAxis.ticks(0));
 
       bottomAxis = d3.axisBottom(xScale)
       svg.append("g")
         .attr("stroke-width",1.8)
         .style("font", "12px times")
-        .attr("class","xscale")
+        .attr("color","white")
         .attr("transform","translate("+(margin.left)+","+(chartheight + margin.top)+")")
         .call(bottomAxis.ticks(0));
 
-        //370
+      //Floating Balls Next
       rScale = d3.scaleLinear().domain([loMin, loMax]).range([1, 15]);
 
-      by = 240
-      bx = 30
+      bx = 220
+      by = 30
 
       for(k=0;k<5;k++){
-
-        ra=Math.floor((Math.random() * musicdata.length) + 1)
         svg.append("circle")
-        .attr("cx",by)
-        .attr("cy",bx)
-        .attr("r",rScale(musicdata[ra].loudness))
-        .style("fill",colorScale(musicdata[ra].liveness))
-        .attr("opacity",1);
+          .attr("cx",bx)
+          .attr("cy",by)
+          .attr("r",rScale(musicdata[k].loudness))
+          .style("fill",colorScale(musicdata[k].liveness))
+          .attr("opacity",1);
         svg.append("text")
-        .attr("x",by+20)
-        .attr("y",bx+5)
-        .attr("fill","white")
-        .attr("font-size","10px")
-        .attr("class","font drakeblz")
-        .text(musicdata[ra].song_title);
-        bx = bx + 40
+          .attr("x",bx+20)
+          .attr("y",by+5)
+          .attr("fill","white")
+          .attr("font-size","10px")
+          .attr("class","font drakeblz")
+          .text(musicdata[k].song_title);
+        by = by + 40
       }
 
-
-
-
-
-
+      //FutureViz
 
       svg = d3.select("#KendrickRec");
       svg.attr("width", 250).attr("height", 100);
 
+      //Artist Name
       artists = "Future"
       musicdata = data;
-      top10 = musicdata.filter( d => d['artist'] == artists);
-      top10 = top10.filter( d => d['duration_ms'] != 'NaN');
-      console.log(top10);
-      musicdata = top10;
-      //const test = d3.mean(musicdata, d => d['energy']);
-      //console.log(test)
-      const kmeanlo = d3.mean(top10, d => d['loudness']);
-      const kmeanli = d3.mean(top10, d => d['liveness']);
-      const kmeandu = d3.mean(top10, d => d['duration_ms']);
-      const kmeanen = d3.mean(top10, d => d['energy']);
+      future = musicdata.filter( d => d['artist'] == artists);
+      future = future.filter( d => d['duration_ms'] != 'NaN');
+      musicdata = future;
 
+      //Artist2 Mean Values
+      const kmeanlo = d3.mean(future, d => d['loudness']);
+      const kmeanli = d3.mean(future, d => d['liveness']);
+      const kmeandu = d3.mean(future, d => d['duration_ms']);
+      const kmeanen = d3.mean(future, d => d['energy']);
+
+      //Print Out Artist Info
       svg.append("text")
         .attr("x","10")
         .attr("y","30")
@@ -244,38 +300,36 @@ d3.csv("spotify.csv").then(function(data) {
               .text("AVG energy:" + Math.abs((kmeanen)));
 
 
-      //Balls
+      //Small Viz
       svg = d3.select("#kendrickballs");
       svg.attr("width", 400).attr("height", 250);
 
-      musicdata = top10;
+      musicdata = future;
 
+      //Margins and Chart Height
       margin = { "top": 10, "right": 150, "bottom": 10, "left":10};
       width = svg.attr("width");
       height = svg.attr("height");
       chartwidth = width - margin["left"] - margin["right"];
       chartheight = height - margin["top"]-margin["bottom"];
 
-      yScale = d3.scaleLinear().domain([1, 0]).range([0,230]);
-      xScale = d3.scaleLinear().domain([0, 1000000]).range([0, 230]);
+      //New Scale
       rScale = d3.scaleLinear().domain([loMin, loMax]).range([1, 3]);
-      colorScale = d3.scaleSequential(d3.interpolateCool).domain([0, liMax])
 
-
+      //Select Div Element
       svg.append("g").attr("id","kstuffs")
       svgb2 = d3.select("g#kstuffs");
 
+      //Draw Points
       for(k=0;k < musicdata.length;k++){
         if (musicdata[k].duration_ms != 'NaN' && musicdata[k].energy != 'NaN'){
           svgb2.append("circle")
-
           .attr("cx",xScale(musicdata[k].duration_ms)+margin["left"])
           .attr("cy",yScale(musicdata[k].energy)+margin["top"])
           .attr("r",rScale(musicdata[k].loudness))
           .style("fill", colorScale(musicdata[k].liveness))
-          .attr("opacity",.7);
+          .attr("opacity",1);
             }
-
       }
 
 
@@ -284,45 +338,38 @@ d3.csv("spotify.csv").then(function(data) {
         .attr("stroke-width",1.8)
         .attr("transform","translate("+(margin.left)+","+margin.top+")")
         .style("font", "12px times")
-        .attr("class","yscale")
+        .attr("color","white")
         .call(leftAxis.ticks(0));
 
       bottomAxis = d3.axisBottom(xScale)
       svg.append("g")
         .attr("stroke-width",1.8)
         .style("font", "12px times")
-        .attr("class","xscale")
+        .attr("color","white")
         .attr("transform","translate("+(margin.left)+","+(chartheight + margin.top)+")")
         .call(bottomAxis.ticks(0));
 
-        //370
+      //New R Scale for side
       rScale = d3.scaleLinear().domain([loMin, loMax]).range([1, 15]);
 
-      by = 240
+      by = 220
       bx = 30
 
       for(k=0;k<5;k++){
-
-        ra=Math.floor((Math.random() * 6) + 1)
         svg.append("circle")
-        .attr("cx",by)
-        .attr("cy",bx)
-        .attr("r",rScale(musicdata[ra].loudness))
-        .style("fill",colorScale(musicdata[ra].liveness))
-        .attr("opacity",1);
+          .attr("cx",by)
+          .attr("cy",bx)
+          .attr("r",rScale(musicdata[k].loudness))
+          .style("fill",colorScale(musicdata[k].liveness))
+          .attr("opacity",1);
         svg.append("text")
-        .attr("x",by+20)
-        .attr("y",bx+5)
-        .attr("fill","white")
-        .attr("font-size","10px")
-        .attr("class","font drakeblz")
-        .text(musicdata[ra].song_title);
+          .attr("x",by+20)
+          .attr("y",bx+5)
+          .attr("fill","white")
+          .attr("font-size","10px")
+          .attr("class","font drakeblz")
+          .text(musicdata[k].song_title.substr(0, 11));
         bx = bx + 40
       }
-
-
-
-
-
 
 })
